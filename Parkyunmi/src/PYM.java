1393f2a5b63f5f42ce1e2529e95a7b0cbe34c419
EZEN-145 <EZEN-145@DESKTOP-JQ5BD63>
2022-07-13 15:54:04 +0900

    classmate name

---
+++ Parkyunmi/src/PYM.java
@@ -0,0 +1,15 @@
+
+public class PYM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HWJ, CEJ, LJH");
+		System.out.println("GMJ, KSJ, KJS");
+		System.out.println("NGH, DHJ, SDY");
+		System.out.println("ABG, OGH, LDH");
+		System.out.println("LYB, JYS, JHS");
+		
+	}
+
+}
